d91abf586c88f082cfb70b69d56f1dbbc9abd79f
DVSneuro <david.v.smith@temple.edu>
2020-09-27 20:55:56 -0400

    add new data

--- convertdata.m
+++ convertdata.m
@@ -10,22 +10,22 @@ clear;
 maindir = pwd;
 
 % grab source data
-sourcedatadir = '/Users/tug87422/Dropbox/Projects/Temple/LearningClinical_wPamButler/newdata-2019/';
+sourcedatadir = '/Users/tug87422/Dropbox/Projects/Temple/LearningClinical_wPamButler/newData-Sep2020/';
 sourcedata = dir([sourcedatadir '*.xls*']);
 sourcedata = struct2cell(sourcedata);
 sourcedata = sourcedata(1,1:end);
 
-%test file for hBayesDM
-fid2 = fopen('indata_hBayesDM.tsv','w');
-fprintf(fid2,'subjID\tchoice\toutcome\n');
-datahb = zeros(100,3); % make a matrix with arbitrary number of rows
+% %test file for hBayesDM
+% fid2 = fopen('indata_hBayesDM.tsv','w');
+% fprintf(fid2,'subjID\tchoice\toutcome\n');
+% datahb = zeros(100,3); % make a matrix with arbitrary number of rows
 
 sublist = zeros(length(sourcedata),1); % add subs here, print later
 idx = 0;
 for i = 1:length(sourcedata)
     fname = sourcedata{i};
     data = xlsread(fullfile(sourcedatadir,fname));
-    
+
     % get file name parts
     fname_split = split(fname,'_');
     subnum_str = fname_split{1};
@@ -42,42 +42,42 @@ for i = 1:length(sourcedata)
     fprintf(fid,'%s\n',cHeader);
     fclose(fid);
     
-    % write data to end of file
-    dlmwrite(outfile,data,'-append');
-    
-    % convert data for hBayesDM (this isn't correct, but testing/learning)
-    for t = 1:length(data)
-        if data(t,3) == -99 || data(t,4) == 0 % skip misses and neutral outcomes
-            continue
-        end
-        idx = idx + 1;
-        if strcmp(condition_str,'money')
-            datahb(idx,1) = str2double(subnum_str) + 100; %differentiate file name
-        else
-            datahb(idx,1) = str2double(subnum_str);
-        end
-        % choice
-        c = data(t,3);
-        if data(t,5) == 1 % choosing between 2 and 3 (negative)
-            if c == 2
-                datahb(idx,2) = 1; % bad
-            elseif c == 3
-                datahb(idx,2) = 2; % good
-            end
-        elseif data(t,5) == 0 % choosing between 1 and 3 (positive)
-            if c == 1
-                datahb(idx,2) = 2; % good
-            elseif c == 3
-                datahb(idx,2) = 1; % bad
-            end
-        end
-        datahb(idx,3) = data(t,4);
-    end
+     % write data to end of file
+     dlmwrite(outfile,data,'-append');
+     
+%     % convert data for hBayesDM (this isn't correct, but testing/learning)
+%     for t = 1:length(data)
+%         if data(t,3) == -99 || data(t,4) == 0 % skip misses and neutral outcomes
+%             continue
+%         end
+%         idx = idx + 1;
+%         if strcmp(condition_str,'money')
+%             datahb(idx,1) = str2double(subnum_str) + 100; %differentiate file name
+%         else
+%             datahb(idx,1) = str2double(subnum_str);
+%         end
+%         % choice
+%         c = data(t,3);
+%         if data(t,5) == 1 % choosing between 2 and 3 (negative)
+%             if c == 2
+%                 datahb(idx,2) = 1; % bad
+%             elseif c == 3
+%                 datahb(idx,2) = 2; % good
+%             end
+%         elseif data(t,5) == 0 % choosing between 1 and 3 (positive)
+%             if c == 1
+%                 datahb(idx,2) = 2; % good
+%             elseif c == 3
+%                 datahb(idx,2) = 1; % bad
+%             end
+%         end
+%         datahb(idx,3) = data(t,4);
+%     end
 end
 
-% write out a sublist for later
-dlmwrite('sublist.txt',unique(sublist));
-dlmwrite('indata_hBayesDM.tsv',datahb,'delimiter','\t','-append');
+% % write out a sublist for later
+% dlmwrite('sublist.txt',unique(sublist));
+% dlmwrite('indata_hBayesDM.tsv',datahb,'delimiter','\t','-append');
 
 
 
